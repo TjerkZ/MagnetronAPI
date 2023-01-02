90d68a93d97e18166418ba8b8bbd84d1ed25eb16
TjerkZ <lala@pepr.nl>
2023-01-02 17:08:55 +0100

    add cors

--- MagnetronAPI/Program.cs
+++ MagnetronAPI/Program.cs
@@ -9,6 +9,18 @@ builder.Services.AddDbContext<MagnetonContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("MagnetronContext"));
 });
 
+// CORS Configuration
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(builder =>
+    {
+        builder
+            .AllowAnyOrigin()
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddControllers();
 
 var app = builder.Build();
@@ -16,6 +28,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseAuthorization();
+
+app.UseCors();
 
 app.MapControllers();
 
